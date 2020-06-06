6dfb43d31ee7e58dcd3c8b855b88a71c3fc6fcb7
Ch Qasim Raza <skyscanner4u@gmail.com>
2020-06-06 10:09:39 +0500

    App .js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
+    <div classname="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} classname="App-logo" alt="logo">
+          <p>
+            Hello from Ch Qasim Raza :)
+          </p>
+        </img>
       </header>
-    </div>
+    </div>      
   );
 }
 
